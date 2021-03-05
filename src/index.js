1a12c0a556000b52b7944dd24beffb6ba9cb0429
github-gtfockout <marsik100041@mail.ru>
2021-03-05 12:55:47 +0300

    fix: added check on argument

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
+  if (arguments.length === 0 || matrix.length === 0) {
+    return []
+  }
   let arr = [];
   for (let i = 0; i < matrix.length; i++) {
     if (i % 2 != 0) {
