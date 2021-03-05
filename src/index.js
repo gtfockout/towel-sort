d6e113d189bfcc7784950b8b51a6854df31ff02e
github-gtfockout <marsik100041@mail.ru>
2021-03-05 12:51:38 +0300

    feat: add towel-sort task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,16 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let arr = [];
+  for (let i = 0; i < matrix.length; i++) {
+    if (i % 2 != 0) {
+      matrix[i].reverse();
+    }
+  }
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      arr.push(matrix[i][j]);
+    }
+  }
+  return arr
 }
